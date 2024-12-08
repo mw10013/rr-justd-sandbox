c7d0739392054ca02ce7e4178433aec9933a1c83
Michael Wu <mw10013@gmail.com>
2024-12-07 22:28:37 -0500

    resizable table

--- app/lib/components/ui/index.ts
+++ app/lib/components/ui/index.ts
@@ -2,3 +2,5 @@ export * from './primitive';
 export * from './button';
 export * from './table';
 export * from './checkbox';
+export * from './card';
+export * from './heading';
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
+import { ResizableTable } from "~/lib/components/resizable-table";
 import type { Route } from "./+types/home";
-import { Button } from "~/lib/components/ui";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="p-6 text-purple-600 font-semibold text-lg flex flex-col space-y-4">
       JustD
-      <Button>JustD Button</Button>
+      <ResizableTable />
     </div>
   );
 }
